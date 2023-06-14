1a20528189b6a9a39feca3b3681e5fbf63efa83f
Sanchez Martin <martinsanchez6973@gmail.com>
2023-06-14 14:16:25 -0300

    actualizacion

---
+++ bd_big_bread.sql
@@ -0,0 +1,11 @@
+USE big_bread;
+SHOW TABLES;
+INSERT INTO receta(iD_receta,nombre_receta,descrip_receta,cantidad_por_receta,unidad_rec)
+VALUES(1,"pan","harina 500g 10 g sal 20 g levadura 500 ml agua",10,"kg")
+USE big_bread;
+INSERT INTO receta(iD_receta,nombre_receta,descrip_receta,cantidad_por_receta,unidad_rec)
+VALUES(2,"criollo","harina 500g 10 g sal 20 g levadura 500 ml agua 100g grasa",5,"kg");
+USE big_bread;
+INSERT INTO receta(iD_receta,nombre_receta,descrip_receta,cantidad_por_receta,unidad_rec)
+VALUES(3,"factura","harina 300g 10 g sal 20 g levadura 500 ml agua 100g grasa 100 g azucar",3,"docena");descrip_receta
+USE TABLE receta;
